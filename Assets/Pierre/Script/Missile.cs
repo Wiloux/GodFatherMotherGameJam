638b12fc7d88a0cd8ac310dd2859ed0427723c2f
Wizerdy <grancru2000@gmail.com>
2021-09-09 10:51:05 +0200

    Animations

--- Assets/Pierre/Script/Missile.cs
+++ Assets/Pierre/Script/Missile.cs
@@ -7,7 +7,8 @@ public class Missile : MonoBehaviour {
     [SerializeField] private float radius;
 
     private void OnTriggerEnter2D(Collider2D collision) {
-        if (collision.gameObject.name.Equals("Tilemap")) {
+        //if (collision.gameObject.name.Equals("Tilemap")) {
+        if (GameManager.instance.whatIsGround.Contains(collision.gameObject.layer)) {
             GameManager.instance.terrainDestruction.DestroyTerrain(transform.position, radius);
             Destroy(gameObject);
         }
