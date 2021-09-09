a2a2e0f4fe4bc7ffd9dcc7f7aaf288dd10bfdc4f
Wiloux <54811449+Wiloux@users.noreply.github.com>
2021-09-09 10:41:34 +0200

    Update Missile.cs

--- Assets/Pierre/Script/Missile.cs
+++ Assets/Pierre/Script/Missile.cs
@@ -7,6 +7,12 @@ public class Missile : MonoBehaviour {
     [SerializeField] private float radius;
     public GameObject explosion;
 
+
+    private void Start()
+    {
+        Destroy(gameObject, 10f);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.name.Equals("Tilemap")) {
             GameManager.instance.terrainDestruction.DestroyTerrain(transform.position, radius);
