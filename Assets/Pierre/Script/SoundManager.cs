7b32dc85c320780753ccf8fdc1bb984ef9847e2d
Wiloux <54811449+Wiloux@users.noreply.github.com>
2021-09-10 10:56:57 +0200

    sfx, ultra fort

---
+++ Assets/Pierre/Script/SoundManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource sfxsd;
+    public AudioSource musicsd;
+    public AudioSource UIsd;
+    public AudioClip GameOver;
+
+    public static SoundManager Instance;
+
+    public AudioClip Breaksfx;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void PauseUnPauseMusic(bool pause = false)
+    {
+        if (pause)
+        {
+            musicsd.Pause();
+        }
+        else
+        {
+            musicsd.UnPause();
+        }
+    }
+    public void PlaySoundEffect(AudioClip clip)
+    {
+            sfxsd.pitch = Random.Range(0.9f, 1.1f);
+            sfxsd.PlayOneShot(clip);
+
+        
+    }
+    
+    public void PlaySoundEffectList(List<AudioClip> clips)
+    {
+            sfxsd.pitch = Random.Range(0.9f, 1.1f);
+            sfxsd.PlayOneShot(clips[Random.Range(0, clips.Count)]);
+
+        
+    }
+
+    public void PlayUISoundEffect(AudioClip clip)
+    {
+        UIsd.PlayOneShot(clip);
+    }
+}
